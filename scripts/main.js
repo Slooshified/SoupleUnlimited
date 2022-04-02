39729ffac382f55365289e247f6c7e86d6941535
MinecraftGamer38 <Aidanmlorimer@gmail.com>
2022-04-02 08:12:14 -0700

    Added Some SoupData(will change). Removed Alerts

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 fetch('./scripts/soupdata.json')
     .then(response => response.json())
-    .then(json => {
-        console.log(json);
+    .then(soupdata => {
+        console.log(soupdata);
         let correctsoupvalue = Math.floor(Math.random() * 12 + 1);
 
         // counting the number of guesses
@@ -14,15 +14,18 @@ fetch('./scripts/soupdata.json')
             let soupguess = document.getElementById("guessField").value;
 
             if (soupguess == correctsoupvalue) {
-                alert("You got the soup! You guessed it right after " + guesses + " attempts ");
+                document.getElementById('guessinfo').innerHTML = "You got the soup! You guessed it right after " + guesses + " attempts ";
+                document.getElementById('guessField').value = "";
             }
             else if (soupguess > correctsoupvalue) /* if guessed number is greater
             than actual number*/ {
                 guesses++;
-                alert("That soup was too high on the spectrum. Please try again.");
+                document.getElementById('guessinfo').innerHTML = "That soup was too high on the spectrum. Please try again.";
+                document.getElementById('guessField').value = "";
             } else {
                 guesses++;
-                alert("That soup was too low on the soup spectrum. Please try again.");
+                document.getElementById('guessinfo').innerHTML = "That soup was too low on the soup spectrum. Please try again.";
+                document.getElementById('guessField').value = "";
             }
         }
     })
