0df11deaec0b62a65d5f8b0a61c1ca2c62f6f293
MinecraftGamer38 <Aidanmlorimer@gmail.com>
2022-04-11 18:22:21 -0700

    Added SoupData and made code more efficient

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,7 @@ fetch('./scripts/soupdata.json')
             let soupguess = document.getElementById("guessField").value;
             var guessinfo;
             if (soupguess == correctsoupvalue) {
-                guessinfo = "You got the soup! You guessed it right after " + guesses + " attempts ";
+                guessinfo = `You got the soup! You guessed it right after ${guesses.toString()} attempts `;
             }
             else if (soupguess > correctsoupvalue) /* if guessed number is greater
             than actual number*/ {
