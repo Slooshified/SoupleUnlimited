908058ac9d989f593cdd592e9dd7b04ca2034d36
VerticalSlab <68572869+VerticalSlab@users.noreply.github.com>
2022-04-14 14:06:56 -0700

    Added Basic 'guess by characteristic'

--- scripts/main.js
+++ scripts/main.js
@@ -16,18 +16,29 @@ fetch('./scripts/soupdata.json')
             let soupguess = document.getElementById("guessField").value;
             let soupguessdata = soupdata[--soupguess];
             console.log(soupguessdata);
-            var guessresponce;
-            if (soupguess == correctsoupindex++) {
+            var guessresponce = '';
+            if (soupguessdata.Name === correctsoupdata.Name) {
                 guessresponce = `You got the soup! You guessed it right after ${guesses.toString()} attempts. Reload Page to play again`;
-            }
-            else if (soupguess > correctsoupindex++) /* if guessed number is greater
-            than actual number*/ {
-                guesses++;
-                guessresponce = "That soup was too high on the spectrum. Please try again.";
             } else {
+                if (correctsoupdata.Carb == soupguessdata.Carb) {
+                    guessresponce = `${guessresponce} Soups share same carb(Pasta, NONE, etc.).`;
+                }
+                if (correctsoupdata.Base == soupguessdata.Base) {
+                    guessresponce = `${guessresponce}<br>Soups share same base(Broth, Stew, etc.)`;
+                }
+                if (correctsoupdata.Main.includes(soupguessdata.Main[0]) || correctsoupdata.Main.includes(soupguessdata.Main[0])) {
+                    guessresponce = `${guessresponce}<br>Soups share 1 or more main ingredents`;
+                }
                 guesses++;
-                guessresponce = "That soup was too low on the soup spectrum. Please try again.";
             }
+            // else if (soupguess > correctsoupindex++) /* if guessed number is greater
+            // than actual number*/ {
+            //     guesses++;
+            //     guessresponce = "That soup was too high on the spectrum. Please try again.";
+            // } else {
+            //     guesses++;
+            //     guessresponce = "That soup was too low on the soup spectrum. Please try again.";
+            // }
             document.getElementById('guessresponce').innerHTML = guessresponce;
             document.getElementById('guessField').value = "";
         }
